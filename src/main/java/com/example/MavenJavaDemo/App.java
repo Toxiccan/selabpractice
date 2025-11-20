af3ddca9f9cdbb3fb1580c4175b347e72695fb1d
Akash <akash.musham@gmail.com>
2025-11-20 16:27:33 +0530

    Initial Maven Java project

---
+++ src/main/java/com/example/MavenJavaDemo/App.java
@@ -0,0 +1,8 @@
+package com.example.MavenJavaDemo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Maven Java Build!");
+    }
+}
+
---
+++ src/test/java/com/example/MavenJavaDemo/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.MavenJavaDemo;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
